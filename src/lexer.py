7cb2ad0cd91742d547fc2f585a514f7b97dbb5c2
Alberto <albertopenasdiaz@gmail.com>
2025-05-08 09:41:04 +0200

    Correcciones en el lexer: 0s no significativos, anexo de .token al fichero de salida y agrupación de tokens

--- src/lexer.py
+++ src/lexer.py
@@ -1,138 +1,156 @@
 import ply.lex as lex
 
+
 class ViperLexer:
     def __init__(self, route: str):
         self.lexer = None
         self.input_file = route
-        self.output_file = route.replace(".vip", ".token")
-        #PEQUEÑA DISTINCION PARA REDIRIGIR LOS .token DE LOS CASOS DE PRUEBA
-        if "test_files/valid" in self.output_file or "test_files/invalid" in self.output_file:
+        if route.endswith(".vip"):
+            self.output_file = route.replace(".vip", ".token")
+        else:
+            self.output_file = route + ".token"
+        # PEQUEÑA DISTINCION PARA REDIRIGIR LOS .token DE LOS CASOS DE PRUEBA
+        if (
+            "test_files/valid" in self.output_file
+            or "test_files/invalid" in self.output_file
+        ):
             if "test_files/valid" in self.output_file:
-                self.output_file = self.output_file.replace("test_files/valid", "test_files/outputs")
+                self.output_file = self.output_file.replace(
+                    "test_files/valid", "test_files/outputs"
+                )
             else:
-                self.output_file = self.output_file.replace("test_files/invalid", "test_files/outputs")
+                self.output_file = self.output_file.replace(
+                    "test_files/invalid", "test_files/outputs"
+                )
+
     # Palabras reservadas
     reserved = {
-        'true': 'TRUE',
-        'false': 'FALSE',
-        'int': 'INT_TYPE',
-        'float': 'FLOAT_TYPE',
-        'char': 'CHAR_TYPE',
-        'def': 'DEF',
-        'return': 'RETURN',
-        'type': 'TYPE',
-        'if': 'IF',
-        'else': 'ELSE',
-        'and': 'AND',
-        'or': 'OR',
-        'not': 'NOT',
-        'bool': 'BOOL_TYPE',
-        'while': 'WHILE'
+        "true": "TRUE",
+        "false": "FALSE",
+        "int": "INT_TYPE",
+        "float": "FLOAT_TYPE",
+        "char": "CHAR_TYPE",
+        "def": "DEF",
+        "return": "RETURN",
+        "type": "TYPE",
+        "if": "IF",
+        "else": "ELSE",
+        "and": "AND",
+        "or": "OR",
+        "not": "NOT",
+        "bool": "BOOL_TYPE",
+        "while": "WHILE",
     }
 
     # Lista completa de tokens que maneja el lexer
     tokens = [
-                 'ID',
-                 'DECIMAL',
-                 'BINARY',
-                 'OCTAL',
-                 'HEXADECIMAL',
-                 'FLOAT_CONST', # Aquí se incluye la notación científica
-                 'CHAR_CONST',
-                 'PLUS', 'MINUS', 'TIMES', 'DIVIDE',
-                 'GT', 'LT', 'GE', 'LE', 'EQ',
-                 'EQUALS',
-                 'LPAREN', 'RPAREN',
-                 'LBRACKET', 'RBRACKET',
-                 'LBRACE', 'RBRACE',
-                 'COMMA', 'COLON', 'DOT', 'SEMICOLON',
-                'COMMENT', 'MLCOMMENT',
-                'NEWLINE'
-             ] + list(reserved.values())
+        "ID",
+        "INT",
+        "FLOAT",  # Aquí se incluye la notación científica
+        "CHAR",
+        "PLUS",
+        "MINUS",
+        "TIMES",
+        "DIVIDE",
+        "GT",
+        "LT",
+        "GE",
+        "LE",
+        "EQ",
+        "EQUALS",
+        "LPAREN",
+        "RPAREN",
+        "LBRACKET",
+        "RBRACKET",
+        "LBRACE",
+        "RBRACE",
+        "COMMA",
+        "COLON",
+        "DOT",
+        "SEMICOLON",
+        "COMMENT",
+        "MLCOMMENT",
+        "NEWLINE",
+    ] + list(reserved.values())
 
     # Expresiones regulares para tokens simples
-    t_PLUS       = r'\+'
-    t_MINUS      = r'-'
-    t_TIMES      = r'\*'
-    t_DIVIDE     = r'/'
-    t_GE         = r'>='
-    t_LE         = r'<='
-    t_EQ         = r'=='
-    t_GT         = r'>'
-    t_LT         = r'<'
-    t_EQUALS     = r'='
-    t_LPAREN     = r'\('
-    t_RPAREN     = r'\)'
-    t_LBRACKET   = r'\['
-    t_RBRACKET   = r'\]'
-    t_LBRACE     = r'\{'
-    t_RBRACE     = r'\}'
-    t_COMMA      = r','
-    t_COLON      = r':'
-    t_SEMICOLON  = r';'
-    t_DOT        = r'\.'
+    t_PLUS = r"\+"
+    t_MINUS = r"-"
+    t_TIMES = r"\*"
+    t_DIVIDE = r"/"
+    t_GE = r">="
+    t_LE = r"<="
+    t_EQ = r"=="
+    t_GT = r">"
+    t_LT = r"<"
+    t_EQUALS = r"="
+    t_LPAREN = r"\("
+    t_RPAREN = r"\)"
+    t_LBRACKET = r"\["
+    t_RBRACKET = r"\]"
+    t_LBRACE = r"\{"
+    t_RBRACE = r"\}"
+    t_COMMA = r","
+    t_COLON = r":"
+    t_SEMICOLON = r";"
+    t_DOT = r"\."
 
     # Ignorar espacios y tabulaciones (de momento)
-    t_ignore = ' \t'
+    t_ignore = " \t"
 
     # Definición para números en coma flotante (incluye notación científica)
-    def t_FLOAT_CONST(self, t):
-        r'\d+\.\d+([e][+-]?\d+)?|\d+[e][+-]?\d+'
+    def t_FLOAT(self, t):
+        r'(?:[1-9]\d*|0)\.\d+(?:[eE][+-]?\d+)?|\d+[eE][+-]?\d+'
         t.value = float(t.value)
         return t
 
-    # Hexadecimal: 0x con letras mayúsculas
-    def t_HEXADECIMAL(self, t):
-        r'0x[0-9A-F]+'
-        t.value = int(t.value, 16)
-        return t
-
-    # Binario: 0b
-    def t_BINARY(self, t):
-        r'0b[01]+'
-        t.value = int(t.value, 2)
-        return t
-
-    # Octal: 0o
-    def t_OCTAL(self, t):
-        r'0o[0-7]+'
-        t.value = int(t.value, 8)
-        return t
-
-    # Decimal
-    def t_DECIMAL(self, t):
-        r'\d+'
-        t.value = int(t.value)
+    def t_INT(self, t):
+        r"""
+        0x[0-9A-F]+ |
+        0b[01]+ |
+        0o[0-7]+ |
+        [1-9]\d* |
+        0
+        """
+        value = t.value
+        if value.startswith("0x"):
+            t.value = int(value, 16)
+        elif value.startswith("0b"):
+            t.value = int(value, 2)
+        elif value.startswith("0o"):
+            t.value = int(value, 8)
+        else:
+            t.value = int(value)
         return t
 
     # Comentarios de una línea
     def t_COMMENT(self, t):
-        r'\#.*'
+        r"\#.*"
         pass
 
     def t_MULTICOMMENT(self, token):
-        r'\'\'\'(.|\n)*\'\'\''
-        token.lexer.lineno += token.value.count('\n')
+        r"\'\'\'(.|\n)*\'\'\'"
+        token.lexer.lineno += token.value.count("\n")
         pass
 
     # Carácter: cualquier símbolo ASCII-extendido delimitado por comillas simples
-    def t_CHAR_CONST(self, t):
-        r'\'([^\\\n]|(\\.))?\''
+    def t_CHAR(self, t):
+        r"'(?:[\x00-\xff]|\\.)'"
         # Se descartan las comillas simples
         t.value = t.value[1:-1]
         return t
 
     # Identificadores y palabras reservadas
     def t_ID(self, t):
-        r'[A-Za-z_][A-Za-z0-9_]*'
-        t.type = self.reserved.get(t.value, 'ID')
+        r"[A-Za-z_][A-Za-z0-9_]*"
+        t.type = self.reserved.get(t.value, "ID")
         return t
 
     # Manejo de saltos de línea para llevar la cuenta
     def t_NEWLINE(self, token):
-        r'\n+'
+        r"\n+"
         token.lexer.lineno += token.value.count("\n")
-        token.value = "nl" # Esto es para poder imprimirlo como "\n" en el archivo de salida .token
+        token.value = "nl"  # Esto es para poder imprimirlo como "\n" en el archivo de salida .token
         return token
 
     # Manejo de errores léxicos
