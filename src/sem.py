808a0eb0b228e564016d056c2e78a50542a0804a
hector-portatl <100495793@alumnos.uc3m.es>
2025-05-12 18:44:42 +0200

    Liegro cambio

---
+++ src/sem.py
@@ -0,0 +1,10 @@
+class Semantic:
+    def __init(self):
+        self.__symbol_table = {}
+
+        """self.__current_function = None
+        self.__current_scope = None
+        self.__current_register = 0
+        self.__current_vector = 0
+        self.__current_variable = 0
+        self.__current_function = 0"""
